c703f31d2656e6ca6309cf11784e767766ecc65b
GraceUmutesi <tesigrace98@gmail.com>
2019-09-13 09:41:12 +0200

    create instance folder

--- app/test_news.py
+++ app/test_news.py
@@ -3,20 +3,3 @@ from models import news
 
 # News = news.News
 
-class newsTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the News class
-    '''
-
-    def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_news = News('CNN','CNN News','Cable News Newtork that is a leader in providing news worldwide','cnn.com','general','U.S.A','en')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_news,News))
-
-
-if __name__ == '__main__':
-    unittest.main()
